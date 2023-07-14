c05d396aa5e1bebf4763ee18caefa6f9df229b26
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-07-14 16:05:05 +0300

    Настроен rate limiter

---
+++ utils/config.js
@@ -0,0 +1,16 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+});
+
+const devDatabaseUrl = 'mongodb://localhost:27017/moviesdb';
+
+const devJwtKey = 'dev-key';
+
+module.exports = {
+  limiter,
+  devDatabaseUrl,
+  devJwtKey,
+};
