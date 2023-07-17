a2dcd1be0e04b49aa18a56281d8b07c2dee8aa20
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-07-17 12:29:12 +0300

    все сохранённые пользователем фильмы;

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,9 +9,8 @@ const {
 
 const getMovies = (req, res, next) => {
   movieModel
-    .find({})
-    .populate('owner')
-    .then((movies) => res.send(movies.reverse()))
+    .find({ owner: req.user._id })
+    .then((movies) => res.send(movies))
     .catch(next);
 };
 
@@ -47,9 +46,7 @@ const createMovie = (req, res, next) => {
       owner,
     })
     .then((movie) =>
-      movie
-        .populate('owner')
-        .then((popMovie) => res.status(ERROR_CODE.CREATED).send(popMovie)),
+      movie.then((popMovie) => res.status(ERROR_CODE.CREATED).send(popMovie)),
     )
     .catch((err) => {
       if (err instanceof ValidationError) {
